caba6ceab6188d2f39d9e9c98dab2d8833cd83ad
Paolo <diciaula.paolo@gmail.com>
2022-03-22 16:14:17 +0100

    fix: add configuration

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ fi
 
 if [[ "$ENV" == "prod" ]]; then
   aws s3 rm s3://prod.anton.webapp.fe --recursive && aws s3 sync ./dist/anton-webapp/ s3://prod.anton.webapp.fe --delete
-  aws cloudfront create-invalidation --distribution-id 	E3MBFSLJ4QSSNO --paths '/*'
+  aws cloudfront create-invalidation --distribution-id E2JO6RD01Z83Z6 --paths '/*'
 fi
