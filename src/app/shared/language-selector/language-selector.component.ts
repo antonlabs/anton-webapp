01e17bc15061e0d58e57dc58a475d3744ff47ecc
Paolo <diciaula.paolo@gmail.com>
2022-04-04 13:18:32 +0200

    fix: closed orders not show up

--- src/app/shared/language-selector/language-selector.component.ts
+++ src/app/shared/language-selector/language-selector.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {rack} from "../../states/app-state";
-import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-language-selector',
